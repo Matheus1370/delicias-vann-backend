4a9abd563ae233f05ffb7ff5785dd0409cce4455
Matheus <matheus31.silva07@gmail.com>
2026-05-12 23:18:21 -0300

    fix(order): registrar modulos que OrderService injeta
    OrderModule nao importava NotificationModule/AuditModule/EntregaModule/
    CreditoModule/IndicacaoModule/EmpresaModule/SazonalModule mesmo apos
    OrderService passar a injetar esses servicos nas fases 3-5. Testes
    unitarios nao pegaram porque mockam todas as dependencias; so apareceu
    em runtime no docker compose com "Nest can't resolve dependencies of
    the OrderService ... EntregaService at index [6]".

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -7,12 +7,26 @@ import { JobsScheduler } from './jobs.scheduler';
 import { CapacityModule } from '../capacity/capacity.module';
 import { CupomModule } from '../cupom/cupom.module';
 import { PaymentModule } from '../payment/payment.module';
+import { NotificationModule } from '../notification/notification.module';
+import { AuditModule } from '../audit/audit.module';
+import { EntregaModule } from '../entrega/entrega.module';
+import { CreditoModule } from '../credito/credito.module';
+import { IndicacaoModule } from '../indicacao/indicacao.module';
+import { EmpresaModule } from '../empresa/empresa.module';
+import { SazonalModule } from '../sazonal/sazonal.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({ name: 'orders' }),
     CapacityModule,
     CupomModule,
+    NotificationModule,
+    AuditModule,
+    EntregaModule,
+    CreditoModule,
+    IndicacaoModule,
+    EmpresaModule,
+    SazonalModule,
     forwardRef(() => PaymentModule),
   ],
   controllers: [OrderController],
